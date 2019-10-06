0feafc46e92f665cade0d8b6313553e3f5c925b8
benceBortnyak <bbortnyak@gmail.com>
2019-10-07 00:20:57 +0200

    ready

--- provision.sh
+++ provision.sh
@@ -4,10 +4,9 @@ sudo useradd -p $(openssl passwd -1 admin) admin -N -g vagrant
     su admin
 
 sed '/Port/d' /etc/ssh/sshd_config
-echo "Port 2222" >> /etc/ssh/sshd_config
-
 sed '/PermitRootLogin/d' /etc/ssh/sshd_config
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "Port 2222" >> /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 
 restart ssh
 
